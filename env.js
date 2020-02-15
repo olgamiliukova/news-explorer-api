a80ae1c55b1b8ac553b3a3f8d95b8fc64c2730c2
Olga Miliukova <cogito_ergo_summ@mail.ru>
2020-02-15 14:45:07 +0300

    Added .env validation

--- env.js
+++ env.js
@@ -1,15 +1,41 @@
+const path = require('path');
 const dotenv = require('dotenv');
 
 module.exports = (app) => {
   const config = dotenv.config();
 
+  if (process.env.NODE_ENV === 'production') {
+    if (config.error) {
+      throw new Error('Config .env is required on production');
+    }
+
+    const distConfig = dotenv.config({
+      path: path.resolve(process.cwd(), '.env.dist'),
+    });
+
+    if (distConfig.error) {
+      throw new Error('Config .env.dist is required on production');
+    }
+
+    // Check for missed keys
+    const missedKeys = Object.keys(distConfig.parsed)
+      .filter(
+        (key) => !(key in config.parsed),
+      );
+    if (missedKeys.length > 0) {
+      throw new Error(
+        'Missed keys for .env: %s'.replace('%s', missedKeys.join(',')),
+      );
+    }
+  }
+
   if (!config.error) {
     const { parsed: env } = config;
 
-    app.set('config', {
-      ...app.get('config'),
+    process.env = {
+      ...process.env,
       ...env,
-    });
+    };
   }
 
   app.set('config', {
--- middlewares/client-ip.js
+++ middlewares/client-ip.js
@@ -2,12 +2,5 @@ const requestIp = require('request-ip');
 
 module.exports = (app) => {
   app.set('trust proxy', true);
-  return (req, _, next) => {
-    req.clientIp = requestIp.getClientIp(req);
-    if (!req.clientIp) {
-      req.clientIp = '127.0.0.1';
-    }
-
-    next();
-  };
+  return requestIp.mw();
 };
